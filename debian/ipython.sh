5ecc194706fce4f48aa5d787a14e60fe7678eef9
piotr <piotr@771dd761-d7fa-0310-a302-f036d1c1ebb6>
2008-05-24 20:21:57 +0000

    Replace /usr/bin/ipython with a shell script which detects desired Python version before invoking IPython. Package depends on default Python version only again (Closes: #476661)
    git-svn-id: svn+ssh://svn.debian.org/svn/python-modules/packages/ipython/trunk@5474 771dd761-d7fa-0310-a302-f036d1c1ebb6

---
+++ debian/ipython.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# determine desired Python version from filename
+VERSION="${0##*ipython}"
+
+if [ ! -f /usr/bin/python$VERSION ]
+then
+	echo "Please install python$VERSION package" >&2
+	exit 1
+else
+	exec python$VERSION -c "import sys; sys.argv[0] = 'ipython$VERSION'; import IPython; IPython.Shell.start().mainloop()" $@
+fi
