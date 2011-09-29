bd6ccb8b5194954804c092a26ba807649f44996e
Yaroslav Halchenko <debian@onerussian.com>
2011-09-29 17:09:18 -0400

    01X: minor changes within misc files to operate on 01x paths

--- debian/ipython01x.sh
+++ debian/ipython01x.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
 # determine desired Python version from filename
-VERSION="${0##*ipython}"
+VERSION="${0##*ipython01x}"
 
 if [ ! -f /usr/bin/python$VERSION ]
 then
 	echo "Please install the python$VERSION package." >&2
 	exit 1
 else
-	exec python$VERSION -c "import sys; sys.argv[0] = '/usr/bin/ipython$VERSION'; from IPython.frontend.terminal.ipapp import launch_new_instance; launch_new_instance()" $@
+	exec python$VERSION -c "import sys; sys.argv[0] = '/usr/bin/ipython01x$VERSION'; from IPython01X.frontend.terminal.ipapp import launch_new_instance; launch_new_instance()" $@
 fi
